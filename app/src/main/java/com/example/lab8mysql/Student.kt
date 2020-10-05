61534d31d84130fb91517e66fdaf34a520e26295
Sophida13 <sophidapr@kkumail.com>
2020-10-05 23:27:12 +0700

    Lab8MySQL_613020511-0_sec3

---
+++ app/src/main/java/com/example/lab8mysql/Student.kt
@@ -0,0 +1,16 @@
+package com.example.lab8mysql
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Student(
+    @Expose
+    @SerializedName("std_id") val std_id: String,
+
+    @Expose
+    @SerializedName("std_name") val std_name: String,
+
+
+    @Expose
+    @SerializedName("std_age") val std_age: Int){}
+
---
+++ app/src/main/java/com/example/lab8mysql/StudentAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.lab8mysql
+
+import android.content.Context
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class StudentAdapter (val item:List<Student>,val context: Context):RecyclerView.Adapter<ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view_item= LayoutInflater.from(parent.context).inflate(R.layout.std_item_layout,parent,false)
+        return ViewHolder(view_item)
+
+
+    }
+
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.tvID.text ="ID : "+item[position].std_id
+        holder.tvName.text =item[position].std_name
+        holder.tvAge.text = "Age : "+item[position].std_age.toString()
+    }
+    override fun getItemCount(): Int {
+
+        return item.size
+    }
+
+}
